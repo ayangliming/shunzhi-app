c910b6544a6bf14fe7cc75fa3c8325458cd0f16d
ayangliming <775391014@example.com>
2017-08-04 17:37:43 +0000

    add svg

--- src/ui/pages/Profile/ProfileEditable.js
+++ src/ui/pages/Profile/ProfileEditable.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react'
-
+import editIcon from './editIcon.svg'
 
 
 
 
 class ProfileEditable extends Component {
+    state={
+      edit:false,
+      slogan:"没有个性签名"
+    }
+    edit=()=>{
+      this.setState({
+        edit:true
+      })
+    }
   render(){
+    console.log(this.state.edit);
+      let editForm=(
+    <form className="profile-form">
+          <input className="profile-slogan-input"
+            ref={value => this.sloganInput = value}
+            type="text"
+            />
+          <button type="submit">保存</button>
+    </form>
+      )
     return(
       <div className="profile">
           <label className="profile-label">
             <input type="file"/>
           </label>
           <div className="profile-username-slogan">
-              <div></div>
+              <div className="profile-username">
+                    123
+              </div>
+              <div className="profile-slogan">
+                  {this.state.edit ? editForm : this.state.slogan}
+              </div>
+          </div>
+          <div className="profile-edit" onClick={this.edit}>
+                <img src={editIcon} alt=""/>
           </div>
       </div>
     )
