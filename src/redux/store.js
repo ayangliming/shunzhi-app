9791a85b449635cfa561781f0fbd88da7198e5ba
ayangliming <775391014@example.com>
2017-08-08 17:22:15 +0000

    add index

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import {createStore} from "redux"
-import rootReducer from "./reduxs/index.js"
-
-const store = createStore(rootReducer)
-
-export default store
--- src/settings.js
+++
@@ -1,6 +0,0 @@
-
-const Settings = {
-  host: 'http://yummy.haoduoshipin.com'
-}
-
-export default Settings
